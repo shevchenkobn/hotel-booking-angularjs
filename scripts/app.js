83e094287f264029a13adaf20579e68efc5597bb
Bogdan Shevchenko <bohdan.shevchenko1@nure.ua>
2017-10-04 17:53:58 +0300

    rebuilding architecture in process

--- scripts/app.js
+++ scripts/app.js
@@ -1,204 +1,24 @@
-
-let suitesInfo = {
-    "rooms": [
-        {
-            type: 1,
-            storey: 3,
-            number: "325",
-            price: 400,
-            description: "A cosy room"
-        },
-        {
-            type: 1,
-            storey: 3,
-            number: "324",
-            price: 400,
-            description: "A cosy room"
-        },
-        {
-            type: 1,
-            storey: 3,
-            number: "323",
-            price: 400,
-            description: "A cosy room"
-        },
-        {
-            type: 1,
-            storey: 3,
-            number: "322",
-            price: 380,
-            description: "A cosy room"
-        },
-        {
-            type: 1,
-            storey: 3,
-            number: "321",
-            price: 390,
-            description: "A cosy room"
-        },
-        {
-            type: 1,
-            storey: 3,
-            number: "320",
-            price: 350,
-            description: "A cosy room"
-        },
-        {
-            type: 1,
-            storey: 3,
-            number: "319",
-            price: 440,
-            description: "A cosy room"
-        },
-        {
-            type: 1,
-            storey: 2,
-            number: "225",
-            price: 320,
-            description: "A cosy room"
-        },
-        {
-            type: 1,
-            storey: 2,
-            number: "224",
-            price: 340,
-            description: "A cosy room"
-        },
-        {
-            type: 1,
-            storey: 2,
-            number: "228",
-            price: 340,
-            description: "A cosy room"
-        },
-        {
-            type: 0,
-            storey: 3,
-            number: "300",
-            price: 250,
-            description: "A cosy room"
-        },
-        {
-            type: 0,
-            storey: 3,
-            number: "301",
-            price: 260,
-            description: "A cosy room"
-        },
-        {
-            type: 0,
-            storey: 3,
-            number: "302",
-            price: 250,
-            description: "A cosy room"
-        },
-        {
-            type: 0,
-            storey: 3,
-            number: "303",
-            price: 250,
-            description: "A cosy room"
-        },
-        {
-            type: 0,
-            storey: 0,
-            number: "18",
-            price: 250,
-            description: "A cosy room"
-        },
-        {
-            type: 0,
-            storey: 0,
-            number: "17",
-            price: 250,
-            description: "A cosy room"
-        },
-        {
-            type: 0,
-            storey: 0,
-            number: "16",
-            price: 250,
-            description: "A cosy room"
-        },
-        {
-            type: 2,
-            storey: 3,
-            number: "340",
-            price: 650,
-            description: "A cosy room"
-        },
-        {
-            type: 2,
-            storey: 3,
-            number: "341",
-            price: 650,
-            description: "A cosy room"
-        },
-        {
-            type: 2,
-            storey: 3,
-            number: "342",
-            price: 650,
-            description: "A cosy room"
-        },
-        {
-            type: 2,
-            storey: 1,
-            number: "180",
-            price: 630,
-            description: "A cosy room"
-        },
-        {
-            type: 2,
-            storey: 1,
-            number: "181",
-            price: 630,
-            description: "A cosy room"
-        },
-        {
-            type: 2,
-            storey: 1,
-            number: "182",
-            price: 630,
-            description: "A cosy room"
-        },
-        {
-            type: 3,
-            storey: 4,
-            number: "4A",
-            price: 800,
-            description: "A cosy room"
-        },
-        {
-            type: 3,
-            storey: 4,
-            number: "4B",
-            price: 900,
-            description: "A cosy room"
-        },
-        {
-            type: 3,
-            storey: 4,
-            number: "4C",
-            price: 1100,
-            description: "A cosy room"
-        },
-    ],
-    "roomsTypes": [
-        "Single", "Double", "Triple/Family", "Suite"
-    ],
-    "dateLimits": [
-      "","9 Oct 2017"  
-    ],
-    "booked": [
-
-    ]
-};
-
 let app = angular.module('hotel-booking', ['720kb.tooltips', 'ngAnimate', 'ui.router']);
-app.factory('Data', function($http) {
+app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('form', {
+        url: '/form',
+        templateUrl: 'form.html',
+        controller: 'formController'
+    })
+    .state('form.profile', {
+        url: '/profile',
+        templateUrl: 'form-profile.html'
+    })
+    .state('form.room', {
+        url: '/room',
+        templateUrl: 'form-payment.html'
+    })
+
+    $urlRouterProvider.otherwise('/form/profile');
+})
+.factory('Data', function($http) {
     // make an ajax request using $http
-    return suitesInfo;
+    return __suitesInfo;
 })
 .factory('User', function($http) {
     var obj = {};
---
+++ scripts/dummy-data.js
@@ -0,0 +1,195 @@
+let __suitesInfo = {
+    "rooms": [
+        {
+            type: 1,
+            storey: 3,
+            number: "325",
+            price: 400,
+            description: "A cosy room"
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "324",
+            price: 400,
+            description: "A cosy room"
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "323",
+            price: 400,
+            description: "A cosy room"
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "322",
+            price: 380,
+            description: "A cosy room"
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "321",
+            price: 390,
+            description: "A cosy room"
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "320",
+            price: 350,
+            description: "A cosy room"
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "319",
+            price: 440,
+            description: "A cosy room"
+        },
+        {
+            type: 1,
+            storey: 2,
+            number: "225",
+            price: 320,
+            description: "A cosy room"
+        },
+        {
+            type: 1,
+            storey: 2,
+            number: "224",
+            price: 340,
+            description: "A cosy room"
+        },
+        {
+            type: 1,
+            storey: 2,
+            number: "228",
+            price: 340,
+            description: "A cosy room"
+        },
+        {
+            type: 0,
+            storey: 3,
+            number: "300",
+            price: 250,
+            description: "A cosy room"
+        },
+        {
+            type: 0,
+            storey: 3,
+            number: "301",
+            price: 260,
+            description: "A cosy room"
+        },
+        {
+            type: 0,
+            storey: 3,
+            number: "302",
+            price: 250,
+            description: "A cosy room"
+        },
+        {
+            type: 0,
+            storey: 3,
+            number: "303",
+            price: 250,
+            description: "A cosy room"
+        },
+        {
+            type: 0,
+            storey: 0,
+            number: "18",
+            price: 250,
+            description: "A cosy room"
+        },
+        {
+            type: 0,
+            storey: 0,
+            number: "17",
+            price: 250,
+            description: "A cosy room"
+        },
+        {
+            type: 0,
+            storey: 0,
+            number: "16",
+            price: 250,
+            description: "A cosy room"
+        },
+        {
+            type: 2,
+            storey: 3,
+            number: "340",
+            price: 650,
+            description: "A cosy room"
+        },
+        {
+            type: 2,
+            storey: 3,
+            number: "341",
+            price: 650,
+            description: "A cosy room"
+        },
+        {
+            type: 2,
+            storey: 3,
+            number: "342",
+            price: 650,
+            description: "A cosy room"
+        },
+        {
+            type: 2,
+            storey: 1,
+            number: "180",
+            price: 630,
+            description: "A cosy room"
+        },
+        {
+            type: 2,
+            storey: 1,
+            number: "181",
+            price: 630,
+            description: "A cosy room"
+        },
+        {
+            type: 2,
+            storey: 1,
+            number: "182",
+            price: 630,
+            description: "A cosy room"
+        },
+        {
+            type: 3,
+            storey: 4,
+            number: "4A",
+            price: 800,
+            description: "A cosy room"
+        },
+        {
+            type: 3,
+            storey: 4,
+            number: "4B",
+            price: 900,
+            description: "A cosy room"
+        },
+        {
+            type: 3,
+            storey: 4,
+            number: "4C",
+            price: 1100,
+            description: "A cosy room"
+        },
+    ],
+    "roomsTypes": [
+        "Single", "Double", "Triple/Family", "Suite"
+    ],
+    "dateLimits": [
+      "","9 Oct 2017"  
+    ],
+    "booked": [
+
+    ]
+};
