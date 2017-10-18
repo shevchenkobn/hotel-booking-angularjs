0e7cbd62bb2ca8f79b82260869324407171a2494
Nikit22 <mykyta.taushanov@nure.ua>
2017-10-18 10:52:48 +0300

    Old but gold stuff

---
+++ dummy-data.js
@@ -0,0 +1,226 @@
+let __suitesInfo = {
+    "rooms": [
+        {
+            type: 0,
+            storey: 3,
+            number: "300",
+            price: 250,
+            description: 'Measuring 17 square meters, the Single Rooms are spacious and comfortable, with plain, welcoming decor, and simple, serviceable modern furniture. They also feature a good range of technical amenities, and are very reasonably priced. Furniture includes a single bed, a writing desk, side-table, and two chairs.<div><img src="./images/fucking-dummy-single-room-0.jpg"></div>'
+        },
+        {
+            type: 0,
+            storey: 3,
+            number: "301",
+            price: 260,
+            description: 'Comfortable and pleasant rooms feature a full bed, comfortable working space, LCD television and high speed internet connection.<div><img src="./images/fucking-dummy-single-room-1.jpg"></div>'
+        },
+        {
+            type: 0,
+            storey: 3,
+            number: "302",
+            price: 290,
+            description: 'Elegance, simplicity and comfort are the leading characteristics in the interior design of Single Rooms. The rooms are the perfect choice for business purposes without ever having to leave the comfort.<div><img src="./images/fucking-dummy-single-room-3.jpg"></div>'
+        },
+        {
+            type: 0,
+            storey: 3,
+            number: "303",
+            price: 250,
+            description: 'Measuring 17 square meters, the Single Rooms are spacious and comfortable, with plain, welcoming decor, and simple, serviceable modern furniture. They also feature a good range of technical amenities, and are very reasonably priced. Furniture includes a single bed, a writing desk, side-table, and two chairs.<div><img src="./images/fucking-dummy-single-room-0.jpg"></div>'
+        },
+        {
+            type: 0,
+            storey: 0,
+            number: "18",
+            price: 270,
+            description: 'Spacious room defined by the architectural and interior Art Nouveau style. Light and friendly colours create a pleasant atmosphere.<div><img src="./images/fucking-dummy-single-room-2.jpg"></div>'
+        },
+        {
+            type: 0,
+            storey: 0,
+            number: "17",
+            price: 290,
+            description: 'Elegance, simplicity and comfort are the leading characteristics in the interior design of Single Rooms. The rooms are the perfect choice for business purposes without ever having to leave the comfort.<div><img src="./images/fucking-dummy-single-room-3.jpg"></div>'
+        },
+        {
+            type: 0,
+            storey: 0,
+            number: "16",
+            price: 260,
+            description: 'Comfortable and pleasant rooms feature a full bed, comfortable working space, LCD television and high speed internet connection.<div><img src="./images/fucking-dummy-single-room-1.jpg"></div>'
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "325",
+            price: 400,
+            description: 'Situated on the higher floors, featuring unique art work and offering spacious and furnished balconies overlooking the coastline, the rooms are ideal for work, leisure or relaxation<div><img src="./images/fucking-dummy-double-room-1.jpg"></div>'
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "324",
+            price: 400,
+            description: 'Situated on the higher floors, featuring unique art work and offering spacious and furnished balconies overlooking the coastline, the rooms are ideal for work, leisure or relaxation<div><img src="./images/fucking-dummy-double-room-1.jpg"></div>'
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "323",
+            price: 400,
+            description: 'Situated on the higher floors, featuring unique art work and offering spacious and furnished balconies overlooking the coastline, the rooms are ideal for work, leisure or relaxation<div><img src="./images/fucking-dummy-double-room-1.jpg"></div>'
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "322",
+            price: 380,
+            description: 'The Double Room offers a beautiful contemporary look and feel. Your Room is a resort style haven for your holiday with two double beds, a range of amenities and LCD TV.<div><img src="./images/fucking-dummy-double-room-0.jpg"></div>'
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "321",
+            price: 380,
+            description: 'The Double Room offers a beautiful contemporary look and feel. Your Room is a resort style haven for your holiday with two double beds, a range of amenities and LCD TV.<div><img src="./images/fucking-dummy-double-room-0.jpg"></div>'
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "320",
+            price: 380,
+            description: 'The Double Room offers a beautiful contemporary look and feel. Your Room is a resort style haven for your holiday with two double beds, a range of amenities and LCD TV.<div><img src="./images/fucking-dummy-double-room-0.jpg"></div>'
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "319",
+            price: 440,
+            description: 'Stay in the heart of our resort, unwind while you admire garden and resort views from your own private balcony. Featuring two double beds, our Double Superior View Room was recently refurbished and features fresh crisp linens, modern furniture, natural lighting and modern décor.<div><img src="./images/fucking-dummy-double-room-2.jpg"></div>'
+        },
+        {
+            type: 1,
+            storey: 2,
+            number: "225",
+            price: 440,
+            description: 'Stay in the heart of our resort, unwind while you admire garden and resort views from your own private balcony. Featuring two double beds, our Double Superior View Room was recently refurbished and features fresh crisp linens, modern furniture, natural lighting and modern décor.<div><img src="./images/fucking-dummy-double-room-2.jpg"></div>'
+        },
+        {
+            type: 1,
+            storey: 2,
+            number: "224",
+            price: 380,
+            description: 'The Double Room offers a beautiful contemporary look and feel. Your Room is a resort style haven for your holiday with two double beds, a range of amenities and LCD TV.<div><img src="./images/fucking-dummy-double-room-0.jpg"></div>'
+        },
+        {
+            type: 1,
+            storey: 2,
+            number: "228",
+            price: 380,
+            description: 'The Double Room offers a beautiful contemporary look and feel. Your Room is a resort style haven for your holiday with two double beds, a range of amenities and LCD TV.<div><img src="./images/fucking-dummy-double-room-0.jpg"></div>'
+        },        
+        {
+            type: 2,
+            storey: 3,
+            number: "340",
+            price: 650,
+            description: 'One Double Bed and One Single Bed; This room measures 214 square feet (20 square meters ). The LCD TV offers cable channels. A minibar, a coffee/tea maker are provided. The private bathroom has shower, hairdryer, room slippers, bathrobes, designer toiletries and complimentary toiletries. Free Internet WI FI Connection, air conditioning, a safe box and a wake-up call service are among the conveniences offered.<div><img src="./images/fucking-dummy-triple-room-0.jpg"></div>'
+        },
+        {
+            type: 2,
+            storey: 3,
+            number: "341",
+            price: 650,
+            description: 'One Double Bed and One Single Bed; This room measures 214 square feet (20 square meters ). The LCD TV offers cable channels. A minibar, a coffee/tea maker are provided. The private bathroom has shower, hairdryer, room slippers, bathrobes, designer toiletries and complimentary toiletries. Free Internet WI FI Connection, air conditioning, a safe box and a wake-up call service are among the conveniences offered.<div><img src="./images/fucking-dummy-triple-room-0.jpg"></div>'
+        },
+        {
+            type: 2,
+            storey: 3,
+            number: "342",
+            price: 650,
+            description: 'One Double Bed and One Single Bed; This room measures 214 square feet (20 square meters ). The LCD TV offers cable channels. A minibar, a coffee/tea maker are provided. The private bathroom has shower, hairdryer, room slippers, bathrobes, designer toiletries and complimentary toiletries. Free Internet WI FI Connection, air conditioning, a safe box and a wake-up call service are among the conveniences offered.<div><img src="./images/fucking-dummy-triple-room-0.jpg"></div>'
+        },
+        {
+            type: 2,
+            storey: 1,
+            number: "180",
+            price: 630,
+            description: 'Our comfortable hotel rooms are ideal to relax in, after a day of discovering sights or a busy day of meetings. Each bedroom is tastefully decorated, warm and inviting. One of the leading hotels in Galway the perfect location for your City break. Free Wi-Fi, TV, en suite bathroom, tea/coffee making facilities feature in this room. Accommodates 3 adults.<div><img src="./images/fucking-dummy-triple-room-1.jpg"></div>'
+        },
+        {
+            type: 2,
+            storey: 1,
+            number: "181",
+            price: 630,
+            description: 'Our comfortable hotel rooms are ideal to relax in, after a day of discovering sights or a busy day of meetings. Each bedroom is tastefully decorated, warm and inviting. One of the leading hotels in Galway the perfect location for your City break. Free Wi-Fi, TV, en suite bathroom, tea/coffee making facilities feature in this room. Accommodates 3 adults.<div><img src="./images/fucking-dummy-triple-room-1.jpg"></div>'
+        },
+        {
+            type: 2,
+            storey: 1,
+            number: "182",
+            price: 630,
+            description: 'Our comfortable hotel rooms are ideal to relax in, after a day of discovering sights or a busy day of meetings. Each bedroom is tastefully decorated, warm and inviting. One of the leading hotels in Galway the perfect location for your City break. Free Wi-Fi, TV, en suite bathroom, tea/coffee making facilities feature in this room. Accommodates 3 adults.<div><img src="./images/fucking-dummy-triple-room-1.jpg"></div>'
+        },
+        {
+            type: 3,
+            storey: 4,
+            number: "4A",
+            price: 800,
+            description: 'Relax on your private, furnished balcony or terrace and enjoy views of the resort gardens. Room features one king bed or two full-size beds, 42-inch LCD HD TV, sitting area, executive desk and a granite- and marble-appointed bathroom with separate vanity area. Amenities include an iPod dock/clock radio, bathrobes, hair dryer, laptop safe, coffee maker and mini refrigerator.<div><img src="./images/fucking-dummy-suite-room-0.jpg"></div>'
+        },
+        {
+            type: 3,
+            storey: 4,
+            number: "4B",
+            price: 900,
+            description: 'These rooms boast a comfortable seating area designed with both traditional and contemporary twists and some have large bay windows or window seats. Junior Suites overlooking Adams Row have a more contemporary feel, with cutting-edge modern art, full-height windows and walnut parquet flooring. Whether in a contemporary or classical Deluxe Junior Suite, all feature the best in state-of-the-art technology such as Bose docking stations and flatscreen TVs.<div><img src="./images/fucking-dummy-suite-room-1.jpg"></div>'
+        },
+        {
+            type: 3,
+            storey: 4,
+            number: "4C",
+            price: 1100,
+            description: 'Furnished with luxurious super king size beds, our Deluxe Suites are meticulously designed to provide the extra space and luxury that can turn a great holiday into a perfect one.<div><img src="./images/photo.jpg"></div>'
+        },
+    ],
+    "roomsTypes": [
+        "Single", "Double", "Triple/Family", "Suite"
+    ],
+    "dateLimits": [
+      "","29 Oct 2017"  
+    ],
+    "booked": [
+        {
+            type: 1,
+            storey: 3,
+            number: "325",
+            price: 400,
+            description: 'A cosy room<div><img src="./images/photo.jpg"></div>',
+            date: new Date("11 Oct 2017")
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "324",
+            price: 400,
+            description: 'A cosy room<div><img src="./images/photo.jpg"></div>',
+            date: new Date("13 Oct 2017")
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "325",
+            price: 400,
+            description: 'A cosy room<div><img src="./images/photo.jpg"></div>',
+            date: new Date("10 Oct 2017")
+        },
+        {
+            type: 1,
+            storey: 3,
+            number: "324",
+            price: 400,
+            description: 'A cosy room<div><img src="./images/photo.jpg"></div>',
+            date: new Date("12 Oct 2017")
+        },
+    ]
+};
